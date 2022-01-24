ddd8d96cd4df77108c08043b6deac6b1ddd8d99a
tomnguyen2 <98356452+tomnguyen2@users.noreply.github.com>
2022-01-24 13:11:49 -0800

    fixed script tag location

---
+++ Tom Nguyen-FactFinder/script.js
@@ -0,0 +1,38 @@
+
+function factOne(){
+	var x = "Glaciers and ice sheets hold about 69 percent of the world's freshwater.";
+	document.getElementById('fun').innerHTML = "<h3>" + x + "</h3> <img src='images/Ice.jpg'>";
+}
+function factTwo(){
+	var y = "The entire world's population could fit inside Los Angeles.";
+	document.getElementById('fun').innerHTML = "<h3>" + y + "</h3>  <img src='images/LA.jfif'>";
+}
+function factThree(){
+	var z = "The smallest country in the world is Vatican City, with a landmass of just 0.49 square kilometers (0.19 square miles).";
+	document.getElementById('fun').innerHTML = "<h4>" + z + "</h4> <img src='images/Vatican-hackathon-FOOKJG.jpg'>";
+}
+function factFour(){
+	var a = "Vietnam is home to the world’s largest cave, Hang Son Doong";
+	document.getElementById('fun').innerHTML = "<h3>" + a + "</h3> <img src='images/sondoongcavebanner-amp__637054714238858623.jpg'>";
+}
+
+function factFive(){
+	var b = "Americans have a troubling understanding of computer code.";
+	document.getElementById('fun').innerHTML = "<h3>" + b + "</h3> <img src='images/American.jpg'>";
+}
+
+function goDSG(){
+	document.querySelector('body').style.backgroundColor = "#8FBC8F";
+}
+
+
+function goOrange(){
+	document.querySelector('body').style.backgroundColor = "#FF8C00";
+}
+
+
+function goGold(){
+	document.querySelector('body').style.backgroundColor = "#FFD700";
+}
+
+
